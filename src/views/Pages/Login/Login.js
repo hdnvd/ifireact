42964b19bda2f68a872a700ac97417471f83703f
Hadi AmirNahavandi <Hadi.nahavandi2010@gmail.com>
2020-01-27 10:18:02 +0330

    Add Latest Changes.

--- src/views/Pages/Login/Login.js
+++ src/views/Pages/Login/Login.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
-import Logo from '../../../logo.png'
 import Cookies from 'universal-cookie';
 import moment from 'moment-jalaali'
 
